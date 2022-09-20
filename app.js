60cf0af33521d2f7c991c66b69cb24e725069b35
Nrdias <natan.nrdias@hotmail.com>
2022-09-20 01:45:47 -0300

    test commit

--- app.js
+++ app.js
@@ -7,34 +7,33 @@ const loginbtn = document.getElementById("loginbtn");
 
 // funcao para registrar usuarios
 function register() {
-    var user = document.getElementById("user").value;
-    var password = document.getElementById("password").value;
-    if (users.includes(user)) {
-        alert('User already exist!')
-    } else {
-        users.push(user);
-        passwords.push(password);
-        console.log(users, passwords);
-        alert('Successful register')
-    }
+  var user = document.getElementById("user").value;
+  var password = document.getElementById("password").value;
+  if (users.includes(user)) {
+    alert("User already exist!");
+  } else {
+    users.push(user);
+    passwords.push(password);
+    console.log(users, passwords);
+    alert("Successful register");
+  }
 }
 // funcao para efetuar login no sistema
 function login() {
-    var user = document.getElementById("user").value;
-    var password = document.getElementById("password").value;
-    if (users.includes(user)) {
-        if (passwords.indexOf(password) === users.indexOf(user)) {
-            alert("login successful");
-        } else {
-            alert("wrong password");
-        }
+  var user = document.getElementById("user").value;
+  var password = document.getElementById("password").value;
+  if (users.includes(user)) {
+    if (passwords.indexOf(password) === users.indexOf(user)) {
+      alert("login successful");
     } else {
-        alert("user not found");
+      alert("wrong password");
     }
+  } else {
+    alert("user not found");
+  }
 }
 // atribuindo funcoes aos botoes
 registerbtn.onclick = register;
 loginbtn.onclick = login;
 
-
-
+console.log("app.js loaded");
